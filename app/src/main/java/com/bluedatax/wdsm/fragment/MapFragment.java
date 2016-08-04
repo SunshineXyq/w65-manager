613a9fbd80dc94d1167c02d46de20d26c947d9f1
徐元强 <yuanqiang.xu@bluedatax.com>
2016-08-04 20:40:31 +0800

    complete location half

--- app/src/main/java/com/bluedatax/wdsm/fragment/MapFragment.java
+++ app/src/main/java/com/bluedatax/wdsm/fragment/MapFragment.java
@@ -1,5 +1,6 @@
 package com.bluedatax.wdsm.fragment;
 
+import android.graphics.Color;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
@@ -9,20 +10,63 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioGroup;
 
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.location.AMapLocationListener;
+import com.amap.api.maps.AMap;
+import com.amap.api.maps.LocationSource;
 import com.amap.api.maps.MapView;
+import com.amap.api.maps.model.BitmapDescriptor;
+import com.amap.api.maps.model.BitmapDescriptorFactory;
+import com.amap.api.maps.model.MyLocationStyle;
 import com.bluedatax.wdsm.R;
 
-public class MapFragment extends Fragment {
+public class MapFragment extends Fragment implements LocationSource,AMapLocationListener,
+        RadioGroup.OnCheckedChangeListener {
 
     MapView mMapView = null;
+    private AMap aMap;
+    private OnLocationChangedListener mListener;
+    private AMapLocationClient mLocationClient;
+    private AMapLocationClientOption mLocationOption;
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.activity_map_fragment,null);
         mMapView = (MapView) view.findViewById(R.id.map);
         mMapView.onCreate(savedInstanceState);
+        init();
         return view;
+    }
+
+    /**
+     * 初始化AMap对象
+     */
+    private void init() {
+        if (aMap == null) {
+            aMap = mMapView.getMap();
+            setUpMap();
+        }
+    }
+
+    /**
+     * 设置amap的属性
+     */
+    private void setUpMap() {
+        MyLocationStyle myLocationStyle = new MyLocationStyle();
+        myLocationStyle.myLocationIcon(BitmapDescriptorFactory.fromResource(R.mipmap.location_marker));
+        myLocationStyle.strokeColor(R.color.black);      //设置圆形的边框颜色
+        myLocationStyle.strokeWidth(1.0f);               //设置圆形边框的粗细
+        myLocationStyle.radiusFillColor(Color.argb(100,0,0,180));   //设置圆形的填充颜色
+        aMap.setMyLocationStyle(myLocationStyle);
+        aMap.setLocationSource(this);                    //设置定位监听
+        aMap.getUiSettings().setIndoorSwitchEnabled(true); //设置默认定位按钮是否显示
+        aMap.setMyLocationEnabled(true);
+
+
     }
 
     @Override
@@ -47,5 +91,47 @@ public class MapFragment extends Fragment {
     public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         mMapView.onSaveInstanceState(outState);
+    }
+
+    /**
+     * 激活定位
+     * @param onLocationChangedListener 实现监听
+     */
+
+    @Override
+    public void activate(OnLocationChangedListener onLocationChangedListener) {
+        mListener = onLocationChangedListener;
+        if (mListener == null) {
+            mLocationClient = new AMapLocationClient(getActivity());
+            mLocationOption = new AMapLocationClientOption();
+            mLocationClient.setLocationListener(this);
+            mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
+            mLocationClient.setLocationOption(mLocationOption);
+            mLocationClient.startLocation();
+        }
+    }
+
+
+    /**
+     * 停止定位
+     */
+    @Override
+    public void deactivate() {
+
+    }
+
+    /**
+     * 定位成功后的回调方法
+     * @param aMapLocation  获取定位参数
+     */
+
+    @Override
+    public void onLocationChanged(AMapLocation aMapLocation) {
+
+    }
+
+    @Override
+    public void onCheckedChanged(RadioGroup group, int checkedId) {
+
     }
 }
